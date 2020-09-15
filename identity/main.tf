c24149d125e27e58ea4fa5880af7c25d37afcb08
Tim Miller <tim.miller@risk.lexisnexis.com>
2020-09-14 22:06:35 -0400

    remove unused code

--- identity/main.tf
+++ identity/main.tf
@@ -1,5 +1,4 @@
 resource "helm_release" "identity" {
-  count      = (var.install_type == "helm" ? 1 : 0)
   name       = (var.helm_name != "" ? var.helm_name : "pod-id-${var.identity_name}")
   chart      = "${path.module}/chart"
 
@@ -27,20 +26,4 @@ resource "helm_release" "identity" {
     name  = "azureIdentityBinding.selector"
     value = var.identity_name
   }
-
-}
-
-resource "github_repository_file" "identity_resource" {
-  count      = (var.install_type == "flux_github" ? 1 : 0)
-  repository = var.flux_git_repo
-  branch     = var.flux_git_branch
-  file       = "${var.flux_project_path}/terraform/namespaces/${var.namespace}/identity-${var.identity_name}.yaml"
-  content    = templatefile("${path.module}/identity.yaml.tmpl", {
-                 name = var.identity_name
-                 namespace = var.namespace
-                 flux_run_level = var.flux_run_level
-                 resource_id = var.identity_resource_id
-                 client_id = var.identity_client_id
-               })
-  commit_message = "terraform - AzureIdentity - ${var.namespace}/${var.identity_name}"
 }
--- identity/variables.tf
+++ identity/variables.tf
@@ -1,13 +1,3 @@
-variable "install_type" {
-  description = "method by which to install identity (helm/flux_github)"
-  type        = string
-
-  validation {
-    condition     = can(regex("helm|flux_git", var.install_type))
-    error_message = "The install_type value must be \"helm\" or \"flux_github\"."
-  }
-}
-
 variable "identity_name" {
   description = "name for Azure identity to be used by AAD"
   type        = string
@@ -33,28 +23,4 @@ variable "helm_name" {
   description = "name of helm installation (defaults to pod-id-<identity_name>"
   type        = string
   default     = ""
-}
-
-variable "flux_git_repo" {
-  description = "git repository within organization (github.com/<organization>/<repo>)"
-  type        = string
-  default     = ""
-}
-
-variable "flux_git_branch" {
-  description = "git branch"
-  type        = string
-  default     = "master"
-}
-
-variable "flux_project_path" {
-  description = "flux project path within repository"
-  type        = string
-  default     = ""
-}
-
-variable "flux_run_level" {
-  description = "flux run level for kubernetes resource"
-  type        = number
-  default     = "1"
 }
